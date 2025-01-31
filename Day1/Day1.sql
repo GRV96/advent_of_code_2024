c8ac90b45da6960a5141d3de6c99ea45673c3ad5
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-01-31 00:28:53 -0500

    Day1: sample data loaded

---
+++ Day1/Day1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS DAY1;
+USE DAY1;
+
+CREATE TABLE IF NOT EXISTS Input(
+locationId1 INT,
+locationId2 INT
+);
+
+-- Absolute path required
+LOAD DATA LOCAL INFILE "Day1Sample.txt"
+INTO TABLE Input
+FIELDS TERMINATED BY "   "; -- Three spaces
+
+SELECT *
+FROM Input;
+
+DROP DATABASE DAY1;
