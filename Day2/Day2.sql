984acb44eaad838bcc8435866d5557d2d3d6c2f6
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-02-01 17:32:18 -0500

    The function counts the bad levels.

--- Day2/Day2.sql
+++ Day2/Day2.sql
@@ -11,8 +11,7 @@ CREATE TABLE IF NOT EXISTS report (
     lvl5 INT,
     lvl6 INT,
     lvl7 INT,
-    is_safe INT DEFAULT 0,
-CONSTRAINT boolean CHECK (is_safe = 0 OR is_safe = 1)
+    nb_bad_levels INT
 );
 TRUNCATE report;
 
@@ -45,14 +44,15 @@ SET is_sign_correct = p_expected_sign IS NULL OR SIGN(p_delta) = p_expected_sign
 RETURN is_value_safe AND is_sign_correct;
 END$$
 
-CREATE FUNCTION is_report_safe(p_report_id INT) RETURNS INT
+CREATE FUNCTION count_bad_levels(p_report_id INT) RETURNS INT
 READS SQL DATA
 BEGIN
-DECLARE is_safe INT;
+DECLARE nb_bad_levels INT;
 DECLARE prev_lvl INT;
 DECLARE current_lvl INT;
 DECLARE delta_lvl INT;
 DECLARE expected_sign INT;
+SET nb_bad_levels = 0;
 SET expected_sign = NULL;
 
 SELECT lvl0
@@ -61,10 +61,10 @@ FROM report
 WHERE id = p_report_id;
 
 INSERT INTO safety_steps (id, lvl0) VALUES
-(p_report_id, prev_lvl);
+(p_report_id, nb_bad_levels);
 
 IF prev_lvl IS NULL THEN
-    RETURN TRUE;
+    RETURN nb_bad_levels;
 END IF;
 
 SELECT lvl1
@@ -73,132 +73,146 @@ FROM report
 WHERE id = p_report_id;
 
 IF current_lvl IS NULL THEN
-    RETURN TRUE;
+    RETURN nb_bad_levels;
 END IF;
 
-UPDATE safety_steps
-SET lvl1 = current_lvl
-WHERE id = p_report_id;
-
 SET delta_lvl = current_lvl - prev_lvl;
-SET is_safe = is_delta_safe(delta_lvl, expected_sign);
-SET expected_sign = SIGN(delta_lvl);
+IF NOT is_delta_safe(delta_lvl, expected_sign) THEN
+    SET nb_bad_levels = nb_bad_levels + 1;
+END IF;
 SET prev_lvl = current_lvl;
+SET expected_sign = SIGN(delta_lvl);
+
+UPDATE safety_steps
+SET lvl1 = nb_bad_levels
+WHERE id = p_report_id;
 
 SELECT lvl2
 INTO current_lvl
 FROM report
 WHERE id = p_report_id;
 
-IF current_lvl IS NULL OR is_safe = FALSE THEN
-    RETURN is_safe;
+IF current_lvl IS NULL THEN
+    RETURN nb_bad_levels;
 END IF;
 
-UPDATE safety_steps
-SET lvl2 = current_lvl
-WHERE id = p_report_id;
-
 SET delta_lvl = current_lvl - prev_lvl;
-SET is_safe = is_delta_safe(delta_lvl, expected_sign);
+IF NOT is_delta_safe(delta_lvl, expected_sign) THEN
+    SET nb_bad_levels = nb_bad_levels + 1;
+END IF;
 SET prev_lvl = current_lvl;
+
+UPDATE safety_steps
+SET lvl2 = nb_bad_levels
+WHERE id = p_report_id;
 
 SELECT lvl3
 INTO current_lvl
 FROM report
 WHERE id = p_report_id;
 
-IF current_lvl IS NULL OR is_safe = FALSE THEN
-    RETURN is_safe;
+IF current_lvl IS NULL THEN
+    RETURN nb_bad_levels;
 END IF;
 
-UPDATE safety_steps
-SET lvl3 = current_lvl
-WHERE id = p_report_id;
-
 SET delta_lvl = current_lvl - prev_lvl;
-SET is_safe = is_delta_safe(delta_lvl, expected_sign);
+IF NOT is_delta_safe(delta_lvl, expected_sign) THEN
+    SET nb_bad_levels = nb_bad_levels + 1;
+END IF;
 SET prev_lvl = current_lvl;
+
+UPDATE safety_steps
+SET lvl3 = nb_bad_levels
+WHERE id = p_report_id;
 
 SELECT lvl4
 INTO current_lvl
 FROM report
 WHERE id = p_report_id;
 
-IF current_lvl IS NULL OR is_safe = FALSE THEN
-    RETURN is_safe;
+IF current_lvl IS NULL THEN
+    RETURN nb_bad_levels;
 END IF;
 
-UPDATE safety_steps
-SET lvl4 = current_lvl
-WHERE id = p_report_id;
-
 SET delta_lvl = current_lvl - prev_lvl;
-SET is_safe = is_delta_safe(delta_lvl, expected_sign);
+IF NOT is_delta_safe(delta_lvl, expected_sign) THEN
+    SET nb_bad_levels = nb_bad_levels + 1;
+END IF;
 SET prev_lvl = current_lvl;
+
+UPDATE safety_steps
+SET lvl4 = nb_bad_levels
+WHERE id = p_report_id;
 
 SELECT lvl5
 INTO current_lvl
 FROM report
 WHERE id = p_report_id;
 
-IF current_lvl IS NULL OR is_safe = FALSE THEN
-    RETURN is_safe;
+IF current_lvl IS NULL THEN
+    RETURN nb_bad_levels;
 END IF;
 
-UPDATE safety_steps
-SET lvl5 = current_lvl
-WHERE id = p_report_id;
-
 SET delta_lvl = current_lvl - prev_lvl;
-SET is_safe = is_delta_safe(delta_lvl, expected_sign);
+IF NOT is_delta_safe(delta_lvl, expected_sign) THEN
+    SET nb_bad_levels = nb_bad_levels + 1;
+END IF;
 SET prev_lvl = current_lvl;
+
+UPDATE safety_steps
+SET lvl5 = nb_bad_levels
+WHERE id = p_report_id;
 
 SELECT lvl6
 INTO current_lvl
 FROM report
 WHERE id = p_report_id;
 
-IF current_lvl IS NULL OR is_safe = FALSE THEN
-    RETURN is_safe;
+IF current_lvl IS NULL THEN
+    RETURN nb_bad_levels;
 END IF;
 
-UPDATE safety_steps
-SET lvl6 = current_lvl
-WHERE id = p_report_id;
-
 SET delta_lvl = current_lvl - prev_lvl;
-SET is_safe = is_delta_safe(delta_lvl, expected_sign);
+IF NOT is_delta_safe(delta_lvl, expected_sign) THEN
+    SET nb_bad_levels = nb_bad_levels + 1;
+END IF;
 SET prev_lvl = current_lvl;
+
+UPDATE safety_steps
+SET lvl6 = nb_bad_levels
+WHERE id = p_report_id;
 
 SELECT lvl7
 INTO current_lvl
 FROM report
 WHERE id = p_report_id;
 
-IF current_lvl IS NULL OR is_safe = FALSE THEN
-    RETURN is_safe;
+IF current_lvl IS NULL THEN
+    RETURN nb_bad_levels;
 END IF;
 
-UPDATE safety_steps
-SET lvl7 = current_lvl
-WHERE id = p_report_id;
-
 SET delta_lvl = current_lvl - prev_lvl;
-SET is_safe = is_delta_safe(delta_lvl, expected_sign);
+IF NOT is_delta_safe(delta_lvl, expected_sign) THEN
+    SET nb_bad_levels = nb_bad_levels + 1;
+END IF;
 SET prev_lvl = current_lvl;
 
-RETURN is_safe;
+UPDATE safety_steps
+SET lvl7 = nb_bad_levels
+WHERE id = p_report_id;
+
+RETURN nb_bad_levels;
 END$$
 DELIMITER ;
 
 -- Absolute path required
-LOAD DATA LOCAL INFILE "Day2Puzzle.txt"
+LOAD DATA LOCAL INFILE "Day2Sample.txt"
 INTO TABLE report
 FIELDS TERMINATED BY " "
 (lvl0, lvl1, lvl2, lvl3, lvl4, lvl5, lvl6, lvl7);
 
 UPDATE report
-SET is_safe = is_report_safe(id)
+SET nb_bad_levels = count_bad_levels(id)
 WHERE id IS NOT NULL;
 
 SELECT *
@@ -211,8 +225,14 @@ SET @puzzle1_answer = -1;
 SELECT COUNT(*)
 INTO @puzzle1_answer
 FROM report
-WHERE is_safe = TRUE;
+WHERE nb_bad_levels = 0;
 
-SELECT @puzzle1_answer;
+SET @puzzle2_answer = -1;
+SELECT COUNT(*)
+INTO @puzzle2_answer
+FROM report
+WHERE nb_bad_levels <= 1;
+
+SELECT @puzzle1_answer, @puzzle2_answer;
 
 DROP DATABASE IF EXISTS day2;
